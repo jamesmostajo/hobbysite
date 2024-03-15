19d7c3c4f461937e547ef40f8638d04560202031
jamesmostajo <jipmostajo@gmail.com>
2024-03-15 19:10:52 +0800

    added product and product type models

--- merchstore/models.py
+++ merchstore/models.py
@@ -2,5 +2,33 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class ProductType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    product_type = models.ForeignKey(
+        "ProductType", on_delete=models.SET_NULL, null=True, related_name="product_type"
+    )
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("merchstore:product_detail", args=[str(self.pk)])
+
+    class Meta:
+        ordering = ["name"]
