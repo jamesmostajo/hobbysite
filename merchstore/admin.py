9866eb366008bd17b2d1d294005ef43781ac2fd7
jamesmostajo <jipmostajo@gmail.com>
2024-03-15 19:14:20 +0800

    added admin panels for merchstore

--- merchstore/admin.py
+++ merchstore/admin.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+# Register your models here.
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, ProductType
+
+
+class ProductTypeAdmin(admin.ModelAdmin):
+    model = ProductType
+
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+
+
+admin.site.register(ProductType, ProductTypeAdmin)
+admin.site.register(Product, ProductAdmin)
