81969eeed2bb15734c2070f91b12ae64e34ba49c
jamesmostajo <jipmostajo@gmail.com>
2024-03-16 23:22:44 +0800

    added models

--- wiki/models.py
+++ wiki/models.py
@@ -3,4 +3,34 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class ArticleCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        "ArticleCategory",
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="article_category",
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True, null=True)
+
+    class Meta:
+        ordering = [
+            "-created_on",
+        ]
+
+    def __str__(self):
+        return self.title
