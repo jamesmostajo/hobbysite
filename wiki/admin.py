a2558f3fb60cf24a72b18e12abc5383f31f5de2c
jamesmostajo <jipmostajo@gmail.com>
2024-03-16 23:25:00 +0800

    added admin

--- wiki/admin.py
+++ wiki/admin.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+# Register your models here.
 from django.contrib import admin
 
-# Register your models here.
+from .models import Article, ArticleCategory
+
+
+class ArticleCategoryAdmin(admin.ModelAdmin):
+    model = ArticleCategory
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    model = Article
+
+
+admin.site.register(ArticleCategory, ArticleCategoryAdmin)
+admin.site.register(Article, ArticleAdmin)
