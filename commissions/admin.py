a083dc4fe84da0073a6db2bd9b729b9f705f53f5
jamesmostajo <jipmostajo@gmail.com>
2024-03-17 11:13:28 +0800

    added admin panels

--- commissions/admin.py
+++ commissions/admin.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
+# Register your models here.
+# -*- coding: utf-8 -*-
+# Register your models here.
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+# Register your models here.
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment, Commission
+
+
+class CommissionAdmin(admin.ModelAdmin):
+    model = Commission
+
+
+class CommentAdmin(admin.ModelAdmin):
+    model = Comment
+
+
+admin.site.register(Commission, CommissionAdmin)
+admin.site.register(Comment, CommentAdmin)
