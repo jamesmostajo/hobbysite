d660733380723e7d15e13c7b2f3f5759b10f45d8
jamesmostajo <jipmostajo@gmail.com>
2024-03-17 09:41:09 +0800

    added and migrated models

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,42 @@
 # -*- coding: utf-8 -*-
+# Create your models here.
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class ArticleCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        "ArticleCategory",
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="article_category",
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:article_detail", args=[str(self.pk)])
+
+    class Meta:
+        ordering = [
+            "-created_on",
+        ]
