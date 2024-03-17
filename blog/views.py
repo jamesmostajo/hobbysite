90c60cd9eb11063a0654e826249cd7b7a3f00527
jamesmostajo <jipmostajo@gmail.com>
2024-03-17 09:56:56 +0800

    views and templates added

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,28 @@
 # -*- coding: utf-8 -*-
+# Create your views here.
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+# Create your views here.
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Article, ArticleCategory
+
+
+def article_list(request):
+    articles = Article.objects.all()
+    ctx = {"articles_list": articles}
+    return render(request, "article_list.html", ctx)
+
+
+def article(request, pk):
+    article = Article.objects.get(pk=pk)
+    ctx = {
+        "name": article.title,
+        "category": article.category.name,
+        "entry": article.entry,
+        "created_on": article.created_on,
+        "updated_on": article.updated_on,
+    }
+    return render(request, "article_detail.html", ctx)
