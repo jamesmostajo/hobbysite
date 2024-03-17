3e58519d1b7b8dd8e985d14235f65d7bfd14d1fc
jamesmostajo <jipmostajo@gmail.com>
2024-03-17 10:42:02 +0800

    added admin panels

--- forum/admin.py
+++ forum/admin.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
+# Register your models here.
+# -*- coding: utf-8 -*-
+# Register your models here.
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+# Register your models here.
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, PostCategory
+
+
+class PostCategoryAdmin(admin.ModelAdmin):
+    model = PostCategory
+
+
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+
+
+admin.site.register(PostCategory, PostCategoryAdmin)
+admin.site.register(Post, PostAdmin)
